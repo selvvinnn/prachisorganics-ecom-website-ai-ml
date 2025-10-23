8f39dd41a64bbf5b81c04745ab5997ed2b09661e
selvvinnn <selvinfernandes83@gmail.com>
2025-10-23 20:00:58 +0530

    added dbpasssword

--- prachiorganics/settings.py
+++ prachiorganics/settings.py
@@ -162,7 +162,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'tramway.proxy.rlwy.net',
         'PORT': '53906',
     
