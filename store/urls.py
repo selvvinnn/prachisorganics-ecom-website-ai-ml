aa1a6dadc6aaea2cc22886da2d33e8cb2ad95b4a
selvvinnn <selvinfernandes83@gmail.com>
2025-11-02 13:04:19 +0530

    asdadasfafafafasrfsafafasf

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'store'
 
@@ -36,6 +38,9 @@ urlpatterns = [
     path('order-success/', TemplateView.as_view(template_name='store/order_success.html'), name='order_success'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
